80ff3113c12585ddc82315b313545e780f35bef3
redlor <redlor1983@gmail.com>
2018-05-02 07:39:30 +0200

    Removed unused dependencies from paid version

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.udacity.gradle.builtitbigger.free.MainActivityFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
